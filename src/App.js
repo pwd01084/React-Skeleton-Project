48a6fd8c17e79fd55bc6b9bb52894e7b7f1c9839
pwd01084 <pwd01084@gmail.com>
2021-09-03 02:31:05 +0900

    Create Customer.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>asdasdff</h2>
+      
+    </div>
+  );
+}
+
+export default App;
